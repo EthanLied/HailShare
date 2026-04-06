f499193e016b9a238615621576e5487a047ac571
PC <ethanjy5428@gmail.com>
2026-04-06 23:36:06 +0800

    Ride List page

--- Customer/rideList/script.js
+++ Customer/rideList/script.js
@@ -8,11 +8,17 @@ function toggleNavbar() {
     // Grabs all navbar item components
     const navbarItems = document.querySelectorAll(".navbarItem");
 
-    // Triggers the "expand" attribute of the ID
+    // Grabs content div
+    const content = document.getElementById("content")
+
+    // Triggers the "expand" attribute of the Navbar
     navbar.classList.toggle("expand")
     
     // Triggers the "expand" attribute of ALL classes matching "navbarItems" 
     navbarItems.forEach(navbarItems => {
         navbarItems.classList.toggle("expand");
     });
+
+    // Triggers the "expand" attribute of the content
+    content.classList.toggle("expand")
 }
